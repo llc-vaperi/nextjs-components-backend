db75d7ae2d6eafaaf53f510c3fe223622b5a8b56
Goga <g.bagauri@yahoo.com>
2025-11-16 17:40:34 +0400

    connected to mongoDB

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -1,7 +1,35 @@
 //path:/api/
 //description: testFunc
-export const firstFunc = (req, res) => {
+
+import { componentsModel } from "../models/mainComponentsModels.js";
+
+export const firstFunc = async (req, res) => {
   try {
+    const data = {
+      name: "FooterDarkSocial",
+      category: "Footer",
+      description: "Dark footer with social icons and links",
+      tags: ["footer", "dark", "social"],
+      code: "export default function FooterDarkSocial() { return (...); }",
+      aiMeta: {
+        theme: "dark",
+        mood: "modern",
+        target: ["business", "travel"],
+        style: "minimal",
+      },
+      previewUrl: "https://cdn.nextcraft.io/previews/footer-dark.png",
+      author: {
+        id: "user123",
+        name: "Tamuri Tskhvediani",
+      },
+      isApproved: true,
+      embedding: [0.123, -0.45, 0.33],
+    };
+
+    const newData = new componentsModel(data);
+
+    await newData.save();
+
     res.json("new server is started ");
   } catch (error) {
     console.log(error);
---
+++ src/db/mongoDB.js
@@ -0,0 +1,13 @@
+import { createConnection } from "mongoose";
+import { config } from "dotenv";
+config();
+
+export const mainComponentConnection = createConnection(
+  process.env.MONGO_URI_MAINCOMPONENTS
+)
+  .on("connected", () => console.log("DB connect main components"))
+  .on("disconnected", () => console.log("DB disconnected main components"))
+  .on("error", () => {
+    console.log("DB error main components");
+    process.exit(1);
+  });
