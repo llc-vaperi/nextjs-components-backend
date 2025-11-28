a30e89495c0bb697b325d756b840b603cdb4442c
Goga <g.bagauri@yahoo.com>
2025-11-28 22:22:17 +0400

    structure changed like nestjs

--- src/db/mongoDB.js
+++ src/db/mongoDB.js
@@ -5,9 +5,9 @@ config();
 export const mainComponentConnection = createConnection(
   process.env.MONGO_URI_MAINCOMPONENTS
 )
-  .on("connected", () => console.log("DB connect main components"))
-  .on("disconnected", () => console.log("DB disconnected main components"))
+  .on("connected", () => console.log("✔ DB connect main components"))
+  .on("disconnected", () => console.log("❌ DB disconnected main components"))
   .on("error", () => {
-    console.log("DB error main components");
+    console.log("❌ DB error main components");
     process.exit(1);
   });
