6fb8fc0a3fa50d6ce2f8f83411c2432bbb028e6e
Goga <g.bagauri@yahoo.com>
2025-11-29 18:20:24 +0400

    refactor: update connection log message for clarity

--- src/db/mongoDB.js
+++ src/db/mongoDB.js
@@ -5,7 +5,7 @@ config();
 export const mainComponentConnection = createConnection(
   process.env.MONGO_URI_MAINCOMPONENTS
 )
-  .on("connected", () => console.log("✔ DB connect main components"))
+  .on("connected", () => console.log("DB connect main components"))
   .on("disconnected", () => console.log("❌ DB disconnected main components"))
   .on("error", () => {
     console.log("❌ DB error main components");
